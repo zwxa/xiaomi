015b13bdd31a91dae2ff19a34ec5f3cbde76e205
zwxa <593693444@qq.com>
2017-07-15 16:59:46 +0800

    mi
    mi

--- script/ad-img.js
+++ script/ad-img.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function() {
-		var duration = 4000; //每张图片的持续显示时间
+		var duration = 3000; //每张图片的持续显示时间
 		var speed = 2000; //图片切换的动画时间
-		var width = $('#slideshow').width(); //获得单张图片的宽度
+		var flag  = true;
 		var curIndex = 0; //设置当前显示图片的索引值
 		var totalIndex = $('#slideshow > ul > li').length; //获得总的图片数量
 		var timer; //设置一个计时变量
 		$('#slideshow > ul > li').each(function(index) {
-			$(this).css("left", index*width+"px"); //设置轮播图片的横向排列
+			
 			$('#slideshow-nav').append("<span>"+(index+1)+"</span>"); //在导航中添加相应的节点
     });
 		$('#slideshow-nav > span').each(function(index) {
@@ -20,8 +20,10 @@
 		$('#slideshow-nav > span').eq(0).addClass("active"); //设置第一个圆点为active
 		var firstChild = $('#slideshow > ul > li').eq(0).clone(); //将第一张图片复制一份
 		$('#slideshow > ul').append(firstChild); //将该图片添加到列表最末
-		firstChild.css("left", totalIndex*width+"px"); //将复制的第一张图片显示在图片序列最右侧
+		//firstChild.css("left", totalIndex*width+"px"); //将复制的第一张图片显示在图片序列最右侧
+	
 		function move(){
+			if (flag){
 			curIndex++; //使索引值加以1
 			if(curIndex>totalIndex){ //当索引值大于图片总数时
 				curIndex = 1; //表示当前应播放第2张图片
@@ -35,10 +37,24 @@
 			}else{
 				$('#slideshow-nav > span').eq(curIndex).addClass("active"); //在其余情况下，则为当前导航节点添加active类
 			}
-			$('#slideshow > ul').animate({left:width*curIndex*-1+"px"},speed); //为图片序列创建动画
+			
+				$('#slideshow > ul>li').eq(curIndex).fadeIn(speed,function(){
+				}).siblings().css({
+					display: 'none'
+				});
+			
+			}
+			 //为图片序列创建动画
 			timer = setTimeout(move,duration+speed); //设置延迟一定时间后执行move函数，延迟时间等于动画时长加上每张图片的持续显示时间
 		}
+	
 		timer = setTimeout(move,duration); //设置延迟一定时间后执行move函数，延迟时间等于每张图片的持续显示时间
 		
+		$('#slideshow > ul>li').hover(function(){
+			flag = false;
+		},function(){
+			flag = true;
+		})
 	});
+
 	
